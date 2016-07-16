965f9b401f3a786366895a10937a6830e8d836a3
Leoberson Lopes <leobersonlopes@hotmail.com>
2016-07-16 12:28:58 -0300

    configuração para deploy

--- starthello/settings.py
+++ starthello/settings.py
@@ -120,3 +120,19 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+
+#honor
+SECURITY_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PTOTO', 'https')
+
+# -------
+ALLOWED_HOSTS = ['*']
+
+#Static files
+import os 
+#BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = 'staticfiles'
+STATIC_URL = '/static/'
+
+STATIC_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+    )
